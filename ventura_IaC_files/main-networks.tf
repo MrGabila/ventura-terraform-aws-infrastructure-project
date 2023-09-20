9ae98a1231889190ae9ecc9c156b3e56e03cc0e0
TheModeler99 <nyuykimo@gmail.com>
2023-09-20 16:31:16 -0400

    updating network config

--- ventura_IaC_files/main-networks.tf
+++ ventura_IaC_files/main-networks.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "ventura-VPC" {
-  cidr_block       = var.vpc_cidr_block
+  cidr_block = var.vpc_cidr_block
   #instance_tenancy = var.instance_tenancy
   tags = {
     Name = "${var.Name}-VPC"
@@ -9,11 +9,11 @@ resource "aws_vpc" "ventura-VPC" {
 resource "aws_subnet" "ventura-Subnet" {
   for_each = var.subnet_configs
 
-  vpc_id                  = aws_vpc.ventura-VPC.id
-  cidr_block              = each.value.cidr_block
-  availability_zone       = each.value.availability_zone
+  vpc_id            = aws_vpc.ventura-VPC.id
+  cidr_block        = each.value.cidr_block
+  availability_zone = each.value.availability_zone
   tags = {
-    Name = each.key  # Use the subnet name as the Name tag
+    Name = each.key # Use the subnet name as the Name tag
   }
 }
 
@@ -25,7 +25,7 @@ resource "aws_internet_gateway" "ventura-IGW" {
   }
 }
 
-resource "aws_route_table" "ventura-RT" {
+resource "aws_route_table" "ventura-RTb" {
   vpc_id = aws_vpc.ventura-VPC.id
   route {
     cidr_block = "0.0.0.0/0"
@@ -33,16 +33,16 @@ resource "aws_route_table" "ventura-RT" {
   }
 
   tags = {
-    Name = "${var.Name}-RT"
+    Name = "${var.Name}-RTb"
   }
 }
 
-resource "aws_route_table_association" "RT_for_NAT-ALB" {
+resource "aws_route_table_association" "RTb_for_NAT-ALB" {
   subnet_id      = aws_subnet.ventura-Subnet["Ventura-Prod-NAT-ALB-Subnet-1"].id
   route_table_id = aws_route_table.ventura-RT.id
 }
 
-resource "aws_route_table_association" "RT_for_ALB" {
+resource "aws_route_table_association" "RTb_for_ALB" {
   subnet_id      = aws_subnet.ventura-Subnet["Ventura-Prod-ALB-Subnet-2"].id
-  route_table_id = aws_route_table.ventura-RT.id
+  route_table_id = aws_route_table.ventura-RTb.id
 }
--- ventura_IaC_files/varaiable.tf
+++ ventura_IaC_files/varaiable.tf
@@ -4,51 +4,51 @@ variable "Name" {
 }
 
 variable "vpc_cidr_block" {
-    type = string
-    default = "10.0.0.0/16"
+  type    = string
+  default = "10.0.0.0/16"
 }
 
 variable "instance_tenancy" {
-    description = "Used in Env with regulatory compliance and restrictions. Ensures the physical hardwares running your instances is reserved for your account alone"
-    default = "dedicated" 
+  description = "Used in Env with regulatory compliance and restrictions. Ensures the physical hardwares running your instances is reserved for your account alone"
+  default     = "dedicated"
 }
 
 variable "subnet_configs" {
   type = map(object({
-    cidr_block       = string
+    cidr_block        = string
     availability_zone = string
   }))
   default = {
-    Ventura-Prod-NAT-ALB-Subnet-1 = {# Usage for Application Load Balancer and NAT gateway
-      cidr_block       = "10.0.1.0/28"# 11 IPs
+    Ventura-Prod-NAT-ALB-Subnet-1 = {   # Usage for Application Load Balancer and NAT gateway
+      cidr_block        = "10.0.1.0/28" # 11 IPs
       availability_zone = "us-east-1a"
     }
-    Ventura-Prod-ALB-Subnet-2 = {# Usage for Application Load Balancer
-      cidr_block       = "10.0.3.0/28"
+    Ventura-Prod-ALB-Subnet-2 = { # Usage for Application Load Balancer
+      cidr_block        = "10.0.3.0/28"
       availability_zone = "us-east-1b"
     }
-    Ventura-Prod-Web-Subnet-1 = {# Usage for Web agent Servers
-      cidr_block       = "10.0.5.0/23"# 507 IPs
+    Ventura-Prod-Web-Subnet-1 = {       # Usage for Web agent Servers
+      cidr_block        = "10.0.4.0/23" # 507 IPs
       availability_zone = "us-east-1a"
     }
     Ventura-Prod-Web-Subnet-2 = {
-      cidr_block       = "10.0.10.0/23"
+      cidr_block        = "10.0.10.0/23"
       availability_zone = "us-east-1b"
     }
-    Ventura-Prod-App-Subnet-1 = {# Usage for App agent Servers
-      cidr_block       = "10.0.15.0/23"# 507 IPs
+    Ventura-Prod-App-Subnet-1 = {        # Usage for App agent Servers
+      cidr_block        = "10.0.14.0/23" # 507 IPs
       availability_zone = "us-east-1a"
     }
     Ventura-Prod-App-Subnet-2 = {
-      cidr_block       = "10.0.20.0/23"
+      cidr_block        = "10.0.20.0/23"
       availability_zone = "us-east-1b"
     }
-    Ventura-Prod-DB-Subnet-1 = {# Usage for RDS-Mysql DB
-      cidr_block       = "10.0.25.0/27"# 27 IPs
+    Ventura-Prod-DB-Subnet-1 = {         # Usage for RDS-Mysql DB
+      cidr_block        = "10.0.25.0/27" # 27 IPs
       availability_zone = "us-east-1a"
     }
     Ventura-Prod-DB-Subnet-2 = {
-      cidr_block       = "10.0.30.0/27"
+      cidr_block        = "10.0.30.0/27"
       availability_zone = "us-east-1b"
     }
   }
